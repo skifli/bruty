aee4ebe9efa92a3605459ec1660790b3c18d90ef
skifli <121291719+skifli@users.noreply.github.com>
2024-10-01 16:26:47 +0000

    fix: forgot to update server client version

--- bruty_server/src/payload_handlers.rs
+++ bruty_server/src/payload_handlers.rs
@@ -1,7 +1,7 @@
 use crate::{SplitSinkExt, WebSocketSender};
 use futures_util::SinkExt;
 
-const ALLOWED_CLIENT_VERSIONS: &[&str] = &["0.2.4"];
+const ALLOWED_CLIENT_VERSIONS: &[&str] = &["0.2.5"];
 
 /// Checks if the connection is authenticated.
 /// If not, it sends an InvalidSession OP code and closes the connection.
